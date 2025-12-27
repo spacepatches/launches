68a580b550a97c04855f3241676d79c0770206ef
Space Patches <spacepatches.contact@gmail.com>
2025-12-27 15:14:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ let query = supabaseClient
     id,
     mission_name,
     net,
+	mission_description,
+	pad_name,
     location_name,
     rocket_full_name,
     lsp_name,
@@ -84,8 +86,10 @@ function renderLaunches(launches) {
         </tr>
         <tr><td class="mission">${l.mission_name || ""}</td></tr>
 		<tr><td>${date}</td></tr>
+        <tr><td>${l.pad_name || ""}</td></tr>
         <tr><td>${l.location_name || ""}</td></tr>
         <tr><td>${l.rocket_full_name || ""} ${stage.serial_number != null ? ` - ${stage.serial_number}` : ""}${stage.flights != null ? `.${stage.flights}` : ""}</td></tr>
+        <tr><td>${l.mission_description || ""}</td></tr>
         <tr><td class="lsp">${l.lsp_name || ""}</td></tr>
         <tr><td class="small">2025–${l.orbital_launch_attempt_count_year ?? ""}, ${l.lsp_abbrev || ""}–${l.agency_launch_attempt_count_year ?? ""}</td>
         <tr><td class="small">${l.status_abbrev || ""} (${l.orbit_abbrev || ""})</td></tr>
