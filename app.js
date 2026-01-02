676171958efcd1beb496ea90c40d771a69297f31
Space Patches <spacepatches.contact@gmail.com>
2026-01-02 08:53:02 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import os
+/* import os
 from supabase import create_client, Client
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+*/
+
+const SUPABASE_URL = "https://dnrlaowhagxjfjzkoyur.supabase.co";
+const SUPABASE_KEY = "sb_publishable_8Rsg9hKeaur_seeAGVJd8w_H60X9ZVG";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
 
 const grid = document.getElementById("grid");
 const form = document.getElementById("filters");
