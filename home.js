11643b330539bc3f723b9eacc9efeaf555e49a5f
Space Patches <spacepatches.contact@gmail.com>
2026-01-17 08:12:19 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -69,6 +69,7 @@ function renderLatestPatches(launches) {
 	  <img src="${patchUrl}" alt="${patchName}">
 <div class="patch-info">
   <span class="patch-name">${patchName}</span>
+  <span class="patch-date">${date}</span>
 </div>
 
 	`;
