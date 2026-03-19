c4d84c5a5602a83cbf80e4db0a8386c43fdcb403
Space Patches <spacepatches.contact@gmail.com>
2026-03-19 16:54:15 +0100

    Add files via upload

--- latest_live_feed.js
+++ latest_live_feed.js
@@ -87,11 +87,11 @@ function renderLatestVideo(launch) {
 
   // 📝 TESTO DINAMICO
   textContainer.innerHTML = `
-<span style="font-weight: normal;">🛰️ Here you can relive the latest rocket launch feed available:&nbsp;
+<div><span style="font-weight: normal;">🛰️ Here you can relive the latest rocket launch feed available:&nbsp;
       ${launch.lsp_name || ""}, 
       ${launch.rocket_full_name || ""}, 
       ${launch.mission_name || ""}
-    </span>
+    </span></div>
   `;
 
   // 🎥 YOUTUBE
